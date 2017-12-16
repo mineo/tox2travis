921e809d973a6b790c2450dfbc900d2bc7ff6609
Wieland Hoffmann <themineo@gmail.com>
2017-12-16 11:35:46 +0100

    Implement a --fallback-python option
    This is the python version that's used for all environments where the mapping
    from tox to travis python versions is not clear.

--- tox2travis/__main__.py
+++ tox2travis/__main__.py
@@ -6,14 +6,17 @@ import click
 
 
 from .tox2travis import (generate_matrix_specification, get_all_environments,
-                         fill_basepythons, travis_yml_header, travis_yml_footer)
+                         fill_basepythons, travis_yml_header, travis_yml_footer,
+                         ALL_KNOWN_BASEPYTHONS)
 
 
 @click.command()
+@click.option("--fallback-python", type=click.Choice(
+    python.tox_version for python in ALL_KNOWN_BASEPYTHONS))
 @click.argument("outfile", type=click.File("w"))
-def main(outfile):  # noqa: D103
+def main(fallback_python, outfile):  # noqa: D103
     envs = get_all_environments()
-    basepythons = fill_basepythons(envs)
+    basepythons = fill_basepythons(envs, fallback_python)
 
     outfile.write(travis_yml_header())
     for matrix_entry in generate_matrix_specification(basepythons):
