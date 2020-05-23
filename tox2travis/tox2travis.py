030d7d4b002766c0e8a65ff60b2d67500da99da8
Wieland Hoffmann <themineo@gmail.com>
2020-05-23 13:18:23 +0200

    Python 3.8 no longer needs the -dev prefix on travis

--- tox2travis/tox2travis.py
+++ tox2travis/tox2travis.py
@@ -85,12 +85,12 @@ class BasePython:
 # example py37/python3.7 should map to "3.7-dev" at the time of this writing).
 
 #: All CPython versions known to tox
-TOX_CPYTHONS = ["2.6", "2.7", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7"]
+TOX_CPYTHONS = ["2.6", "2.7", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7", "3.8"]
 #: All pypy versions known to tox and travis
 # https://docs.travis-ci.com/user/reference/xenial/#python-support
 TOX_PYPYS    = [BasePython("pypy", "pypy2.7-6.0"), BasePython("pypy2", "pypy2.7-6.0", actions_version="pypy2"), BasePython("pypy3", "pypy3.5-6.0", actions_version="pypy3")]  # noqa: E221,E501
 #: All Python development versions supported by tox and travis
-TOX_DEVPTHONS = [BasePython("python3.8", "3.8-dev", actions_version="3.8")]
+TOX_DEVPTHONS = []
 
 ALL_KNOWN_BASEPYTHONS = [
     BasePython("python{version}".format(version=version), version)
